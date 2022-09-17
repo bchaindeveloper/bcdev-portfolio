74620762dd7e4bb91b34a91dcaae02df9e571168
bchaindeveloper <bcdev3@protonmail.com>
2022-09-17 13:00:42 -0400

    Update data

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -3,12 +3,13 @@ import React from "react";
 import {
   Section,
   SectionDivider,
+  SectionText,
   SectionTitle,
 } from "../../styles/GlobalComponents";
 import { Box, Boxes, BoxNum, BoxText } from "./AcomplishmentsStyles";
 
 const data = [
-  { number: 29, text: "Repositories" },
+  // { number: 29, text: "Repositories" },
   // { number: 1000, text: "Patrick Collins Blockchain Course" },
   // { number: 1900, text: "Github Followers" },
   // { number: 5000, text: "Github Stars" },
@@ -17,14 +18,49 @@ const data = [
 const Acomplishments = () => (
   <Section>
     <SectionTitle>Personal Acomplishments</SectionTitle>
-    <Boxes>
+    <SectionText>
+      My main acomplishments so far are educational based from CodeCademy
+      courses to FreeCodeCamp tutorial. Below are some of my most significant acomplishments.{" "}
+    </SectionText>
+    <SectionText>
+    <div>
+        <h4> Completed: Solidity, Blockchain Python Tutorial</h4>
+        <iframe
+          width="auto"
+          height="auto"
+          src="https://www.youtube.com/embed/M576WGiDBdQ"
+          title="Solidity, Blockchain, and Smart Contract Course – Beginner to Expert Python Tutorial"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <br />
+      <div>
+        <h4>Currently Enrolled:  Solidity, Blockchain, FullStack Web3 JS Tutorial</h4>
+        <iframe
+          width="auto"
+          height="auto"
+          src="https://www.youtube.com/embed/gyMwXuJrbJQ"
+          title="Learn Blockchain, Solidity, and Full Stack Web3 Development with JavaScript – 32-Hour Course"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <br />
+      <div>
+        <h4>Currently Enrolled: CodeCademy_ Full-Stack Software Engineer</h4>
+      </div>
+    </SectionText>
+    {/* <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
           <BoxNum>{card.number}+</BoxNum>
           <BoxText>{card.text}</BoxText>
         </Box>
       ))}
-    </Boxes>
+    </Boxes> */}
   </Section>
 );
 
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  DiFirebase,
+  DiJavascript,
+  DiLinux,
+  DiNodejs,
+  DiPostgresql,
+  DiReact,
+  DiZend,
+} from "react-icons/di";
 import {
   Section,
   SectionDivider,
@@ -25,29 +33,77 @@ const Technologies = () => (
     </SectionText>
     <List>
       <ListItem>
-        <DiReact size="item"/>
+        <img
+          src="https://camo.githubusercontent.com/6831b4de31807bcf9e2c8c25dd19a4f465382cf29cbc9a58dac10960a1799194/68747470733a2f2f736f6c69646974796c616e672e6f72672f696d616765732f536f6c477261792e706e67"
+          width="220px"
+          height="100px"
+        />
+        <br />
+        <ListContainer>
+          <ListTitle>Solidity</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Solidity
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <img
+          src="https://camo.githubusercontent.com/97ab139a10b0a0ef776fdccccc7dc7b8386c8cb3faa3d1797956bb906ca489f8/68747470733a2f2f776562336a732e6f72672f776562336a732e706e67"
+          width="220px"
+          height="100px"
+        />
+        <br />
+        <ListContainer>
+          <ListTitle>Web3.js</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Web3.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <img
+          src="https://camo.githubusercontent.com/2ab62ab50123744ccf49a93413e2397ca1584ca9fae540d1118507db4035f393/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f312f31352f436861696e6c696e6b5f4c6f676f5f426c75652e7376672f33373870782d436861696e6c696e6b5f4c6f676f5f426c75652e7376672e706e67"
+          width="220"
+          height="100px"
+        />
+        <br />
+        <ListContainer>
+          <ListTitle>Chainlink</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Chainlink
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size="item" />
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
-          <ListParagraph>Experience with <br />
-          React.js
+          <ListParagraph>
+            Experience with <br />
+            React.js
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiFirebase size="item"/>
+        <DiNodejs size="item" />
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
-          <ListParagraph>Experience with <br />
-          Node and Databases
+          <ListParagraph>
+            Experience with <br />
+            Node.js
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiFirebase size="item"/>
+        <DiFirebase size="item" />
         <ListContainer>
-          <ListTitle>Blockchain</ListTitle>
-          <ListParagraph>Experience with <br />
-          Solidity
+          <ListTitle>Databases</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Databases
           </ListParagraph>
         </ListContainer>
       </ListItem>
